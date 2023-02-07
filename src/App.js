d334e36e9e7b84dfad0debf7570a02f3e66a3aca
Sky <oloof92@gmail.com>
2023-02-07 15:41:08 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import DocumentMeta from "react-document-meta";
+import { Routes, Route, Outlet, Link } from "react-router-dom";
+
+import Header from "./components/Header";
+import Main from "./pages/Main";
+
+import "./index.css";
+
+const meta = {
+  title: "Princess konditori",
+  description: "Konditori.",
+  meta: {
+    charset: "utf-8",
+    name: {
+      keywords: "fika, café",
+    },
+  },
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <DocumentMeta {...meta}>
+      <Header />
+      <Routes>
+        <Route path={"/"} element={<Layout />}>
+          <Route index element={<Main />} />
+          <Route path="*" element={<NoMatch />} />
+        </Route>
+      </Routes>
+    </DocumentMeta>
+  );
+}
+
+function Layout() {
+  return (
+    <main>
+      <Outlet />
+    </main>
+  );
+}
+
+function NoMatch() {
+  return (
+    <div>
+      <h2>Nothing to see here!</h2>
+      <p>
+        <Link to={process.env.REACT_APP_BASE_URL}>Go to the home page</Link>
+      </p>
     </div>
   );
 }
