fa24b331db953433f580b211d4079dd4ffaba721
Sky <oloof92@gmail.com>
2023-02-15 15:37:55 +0100

    add data

---
+++ src/data/data.js
@@ -0,0 +1,11 @@
+import cake from "../assets/princess-cake.png";
+
+const list = [
+  { img: cake, name: "Princesstårta, rosa", link: "/princess-cake" },
+  { img: cake, name: "princess-cake2", link: "/princess-cake" },
+  { img: cake, name: "princess-cake3", link: "/princess-cake" },
+  { img: cake, name: "princess-cake4", link: "/princess-cake" },
+  { img: cake, name: "princess-cake5", link: "/princess-cake" },
+];
+
+export default list;
