a4220e79c9e35df7f9f9d6017468ca0405895ac3
Sky <oloof92@gmail.com>
2023-02-15 15:36:58 +0100

    init app

--- src/index.js
+++ src/index.js
@@ -1,14 +1,48 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App";
+import { React } from "react";
+import { createRoot } from "react-dom/client";
+import { RouterProvider, Outlet, createBrowserRouter } from "react-router-dom";
+import DocumentMeta from "react-document-meta";
+import Main from "./pages/Main";
+import Header from "./components/Header";
+import NavBar from "./components/NavBar";
+import NoMatch from "./pages/NoMatch";
 import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+const meta = {
+  title: "Princess konditori",
+  description: "Konditori.",
+  meta: {
+    charset: "utf-8",
+    name: {
+      keywords: "fika, café",
+    },
+  },
+};
+
+const AppLayout = () => (
+  <DocumentMeta {...meta}>
+    <Header />
+    <NavBar />
+    <Outlet />
+  </DocumentMeta>
+);
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Main />,
+      },
+      {
+        path: "*",
+        element: <NoMatch />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
 );
