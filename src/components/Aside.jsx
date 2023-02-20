43a942a291a0e97537a06ba65dee541749f531c7
Sky <oloof92@gmail.com>
2023-02-20 15:22:31 +0100

    add aside

---
+++ src/components/Aside.jsx
@@ -0,0 +1,33 @@
+import { React, useEffect, useState } from "react";
+import PageSection from "../components/PageSection";
+import AsideCard from "./cards/AsideCard";
+import { asideData } from "../data/data";
+
+function Aside() {
+  const [asideContent, setAsideContent] = useState([]);
+  const data = asideData;
+
+  useEffect(() => {
+    setAsideContent(data);
+  }, []);
+
+  return (
+    <aside className="hidden 2xl:block">
+      <PageSection title="Se mer">
+        <section className="mt-16 mb-16 grid grid-cols-1 gap-16">
+          {asideContent.map((data, index) => {
+            return (
+              <AsideCard
+                key={index}
+                link={data.link}
+                img={data.img}
+                text={data.text}
+              />
+            );
+          })}
+        </section>
+      </PageSection>
+    </aside>
+  );
+}
+export default Aside;
