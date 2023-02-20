5c96624a7d67d7a1801731bd60ef8d0fcbce6f1c
Sky <oloof92@gmail.com>
2023-02-20 15:22:00 +0100

    add assets

--- src/data/data.js
+++ src/data/data.js
@@ -5,21 +5,63 @@ import chocklad2 from "../assets/chocklad-2.png";
 import princessGreen from "../assets/marsipan.png";
 import shrimpImg from "../assets/shrimp.jpg";
 import coffeeAndBread from "../assets/coffee-and-bread.jpg";
+import brollop3 from "../assets/brollop-3.jpg";
+import brollop4 from "../assets/brollop-4.jpg";
+import krokan1 from "../assets/krokan1.jpeg";
+import krokan2 from "../assets/krokan2.jpeg";
+import krokan3 from "../assets/krokan3.jpeg";
+
+export const asideData = [
+  {
+    img: brollop4,
+    text: "Bröllopstårta i 4 våningar med rosor",
+    link: "/tarta/brollops-tarta-4",
+  },
+  {
+    img: krokan1,
+    text: "Kristopher Petersson gör en stor sexkantig krokan",
+    link: "/bilder/krokan",
+  },
+  {
+    img: brollop3,
+    text: "Bröllopstårta i 3 våningar",
+    link: "/tarta/brollops-tarta-3",
+  },
+  {
+    img: krokan2,
+    text: "Kristopher Petersson gör en stor sexkantig krokan",
+    link: "/bilder/krokan",
+  },
+  {
+    img: brollop4,
+    text: "Bröllopstårta i 4 våningar med rosor",
+    link: "/tarta/brollops-tarta-4",
+  },
+  {
+    img: krokan3,
+    text: "Kristopher Petersson gör en stor sexkantig krokan",
+    link: "/bilder/krokan",
+  },
+];
 
 export const cakeList = [
   {
     img: princessPink,
     name: "Princesstårta, rosa",
-    link: "/princess-cake-pink",
+    link: "/tarta/princess-rosa",
   },
   {
     img: princessGreen,
     name: "Princesstårta, grön",
-    link: "/princess-cake-green",
+    link: "/tarta/princess-gron",
   },
-  { img: budapest, name: "Budapest", link: "/budapest" },
-  { img: chocklad, name: "Chockladtårta", link: "/chocolate-cake" },
-  { img: chocklad2, name: "Chockladtårta, stor", link: "/chocolate-cake-2" },
+  { img: budapest, name: "Budapest", link: "/tarta/budapest" },
+  { img: chocklad, name: "Chockladtårta", link: "/chocklad-tarta" },
+  {
+    img: chocklad2,
+    name: "Chockladtårta, stor",
+    link: "/tarta/chocklad-tarta-2",
+  },
 ];
 
 export const campaignOne = {
