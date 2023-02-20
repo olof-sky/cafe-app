fe108897d564473cd0da3beb0320fe055a5130f2
Sky <oloof92@gmail.com>
2023-02-20 15:22:07 +0100

    add screensize

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,13 @@ module.exports = {
     extend: {
       container: {
         center: true,
+        screens: {
+          sm: "600px",
+          md: "728px",
+          lg: "984px",
+          xl: "1240px",
+          "2xl": "1900px",
+        },
       },
       colors: {
         indigo: "#5c6ac4",
