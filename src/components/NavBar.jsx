85a6dbba9cd620110d51b167f62cd26e7a9c06ac
Sky <oloof92@gmail.com>
2023-02-21 09:14:44 +0100

    add icon to navbar close btn

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,6 +2,7 @@ import { React, useState, Fragment } from "react";
 import { Link } from "react-router-dom";
 import NavButton from "./buttons/NavButton";
 import { RxHamburgerMenu } from "react-icons/rx";
+import { AiOutlineClose } from "react-icons/ai";
 
 function NavBar() {
   const [showNav, setShowNav] = useState(false);
@@ -24,18 +25,25 @@ function NavBar() {
         <NavButton url="/brollop" text="Bröllop" />
       </nav>
       <nav className="fixed z-10 top-0 right-0 lg:hidden pt-6 pr-16">
-        <button className="fixed z-30" onClick={() => setShowNav(!showNav)}>
-          <RxHamburgerMenu className="w-10 h-10 text-yellow" />
-        </button>
         {showNav ? (
-          <div className="fixed flex flex-col items-center gap-6 pt-32 z-20 top-0 left-0 w-screen h-screen bg-black">
-            <LinkButton text="Hem" link="/" />
-            <LinkButton text="Kontakt" link="/kontakt" />
-            <LinkButton text="Tårtor" link="/tartar" />
-            <LinkButton text="Bilder" link="/bilder" />
-            <LinkButton text="Bröllop" link="/brollop" />
-          </div>
-        ) : null}
+          <Fragment>
+            {" "}
+            <button className="fixed z-30" onClick={() => setShowNav(!showNav)}>
+              <AiOutlineClose className="w-10 h-10 text-yellow" />
+            </button>
+            <div className="fixed flex flex-col items-center gap-6 pt-32 z-20 top-0 left-0 w-screen h-screen bg-black">
+              <LinkButton text="Hem" link="/" />
+              <LinkButton text="Kontakt" link="/kontakt" />
+              <LinkButton text="Tårtor" link="/tartar" />
+              <LinkButton text="Bilder" link="/bilder" />
+              <LinkButton text="Bröllop" link="/brollop" />
+            </div>
+          </Fragment>
+        ) : (
+          <button className="fixed z-30" onClick={() => setShowNav(!showNav)}>
+            <RxHamburgerMenu className="w-10 h-10 text-yellow" />
+          </button>
+        )}
       </nav>
     </Fragment>
   );
