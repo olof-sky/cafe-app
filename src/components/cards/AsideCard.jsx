d38e4c39149e792c464d75eb6b9015be51e32ac7
Sky <oloof92@gmail.com>
2023-02-24 14:27:44 +0100

    fix css

--- src/components/cards/AsideCard.jsx
+++ src/components/cards/AsideCard.jsx
@@ -4,7 +4,7 @@ function AsideCard(props) {
   return (
     <article className="bg-red w-56 border-dashed border-b-2 border-yellow">
       <Link to={props.link}>
-        <img className="w-56 h-56 object-fill" alt="/" src={props.img} />
+        <img className="w-56 h-56 object-cover" alt="/" src={props.img} />
         <p className="p-4">{props.text}</p>
       </Link>
     </article>
