245e387f6a03110757926bed37e20f25c9c65c73
Sky <oloof92@gmail.com>
2023-02-28 12:03:14 +0100

    update env

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCIjfy8sQijeOOpd3iZ9z0SNSdeN_LQDuo",
-  authDomain: "cafe-app-e40a2.firebaseapp.com",
-  projectId: "cafe-app-e40a2",
-  storageBucket: "cafe-app-e40a2.appspot.com",
-  messagingSenderId: "281147002737",
-  appId: "1:281147002737:web:20fee13315be1abd15f0d0",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 // Initialize Firebase
